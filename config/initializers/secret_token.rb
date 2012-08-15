966b62765b76e9c313e5abb5e0440a4e0d47a8d1
river2708 <river2708@gmail.com>
2012-08-15 06:59:55 +0300

    clear project with test in gem file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'f90b9c4f7b30e7ca3b7b818c4df53be300b29097b502ea601260318ae1b5bdee918f4eee7aef650e6732ff345fca90bb6056298434a7e7eb8367de9eaea04ca0'
